5441f48f74114a7ac07b8f2afcc1ed6ba825c9da
Burak Serdar <bserdar@redhat.com>
2015-04-11 01:22:18 -0600

    Add null operation

---
+++ src/main/java/com/redhat/lightblue/rdbms/rdsl/NullOperation.java
@@ -0,0 +1,87 @@
+/*
+ Copyright 2013 Red Hat, Inc. and/or its affiliates.
+
+ This file is part of lightblue.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.redhat.lightblue.rdbms.rdsl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.redhat.lightblue.util.Path;
+import com.redhat.lightblue.util.Error;
+
+import com.redhat.lightblue.rdbms.tables.Table;
+
+public class NullOperation implements ScriptOperation, ScriptOperationFactory {
+
+    private static final Logger LOGGER=LoggerFactory.getLogger(NullOperation.class);
+
+    public static final String NAME="$null";
+    public static final String NAMES[]={NAME};
+
+    private Path dest;
+
+    public NullOperation() {}
+
+    public NullOperation(Path dest) {
+        this.dest=dest;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public Value execute(ScriptExecutionContext ctx) {
+        ValueType type=ctx.getVarType(dest);
+        switch(type) {
+        case primitive:
+        case lob:
+        case list:
+        case map:
+            ctx.setVarValue(dest,null);
+            break;
+
+        case table:
+            ((Table)ctx.getVarValue(dest).getValue()).resetColumnValues();
+            break;
+        }
+        return Value.NULL_VALUE;
+    }
+
+    @Override
+    public String[] operationNames() {
+        return NAMES;
+    }
+
+    @Override
+    public ScriptOperation getOperation(OperationRegistry reg,ObjectNode node) {
+        NullOperation newOp=new NullOperation();
+        ObjectNode args=(ObjectNode)node.get(NAME);
+        JsonNode x=args.get("dest");
+        if(x!=null) {
+            newOp.dest=new Path(x.asText());
+        } else {
+            throw Error.get(ScriptErrors.ERR_MISSING_ARG,"dest");
+        }
+        return newOp;
+    }
+}
---
+++ src/test/java/com/redhat/lightblue/rdbms/rdsl/NullOperationTest.java
@@ -0,0 +1,74 @@
+/*
+ Copyright 2013 Red Hat, Inc. and/or its affiliates.
+
+ This file is part of lightblue.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.redhat.lightblue.rdbms.rdsl;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.redhat.lightblue.metadata.EntityMetadata;
+
+import com.redhat.lightblue.util.JsonDoc;
+import com.redhat.lightblue.util.JsonUtils;
+import com.redhat.lightblue.util.Path;
+
+import com.redhat.lightblue.rdbms.tables.Table;
+import com.redhat.lightblue.rdbms.tables.PrimaryKey;
+import com.redhat.lightblue.rdbms.tables.Column;
+
+public class NullOperationTest {
+
+    @Test
+    public void nullTest() throws Exception {
+        EntityMetadata md=TestUtil.getMd("testMetadata.json");
+        JsonDoc doc=TestUtil.getDoc("sample1.json");
+        ScriptExecutionContext ctx=ScriptExecutionContext.getInstanceForInsertion(TestUtil.getTables(md),doc,md);
+
+        // Assign some values to columns
+        ctx.setVarValue(new Path("$tables.schema.table.id1"),new Value(1));
+        ctx.setVarValue(new Path("$tables.schema.table.id2"),new Value(2));
+        ctx.setVarValue(new Path("$tables.schema.table.col1"),new Value(3));
+        ctx.setVarValue(new Path("$tables.schema.table.col2"),new Value(4));
+        ctx.setVarValue(new Path("$tables.schema.table.col3"),new Value(5));
+        ctx.setVarValue(new Path("$tables.schema.table.col4"),new Value(6));
+        ctx.setVarValue(new Path("$tables.schema.table.col5"),new Value(7));
+        ctx.setVarValue(new Path("$tables.schema.table.col6"),new Value(8));
+        ctx.setVarValue(new Path("$tables.schema.table.col7"),new Value(9));
+
+        Script s=new Script(new NullOperation(new Path("$tables.schema.table")));
+        s.execute(ctx);
+
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.id1")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.id2")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.col1")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.col2")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.col3")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.col4")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.col5")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.col6")).getValue());
+        Assert.assertNull(ctx.getVarValue(new Path("$tables.schema.table.col7")).getValue());
+    }
+
+}
